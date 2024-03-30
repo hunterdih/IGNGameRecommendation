cd5635389f617cbe9d5eec0d3df307458fc7d282
dihdd <hunter.davi@northeastern.edu>
2024-03-30 12:32:04 -0400

    Uploading dataset

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-import pandas as pd
-import numpy as np
-from pathlib import Path
+import os
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+chat = ChatGroq(temperature=0, groq_api_key = GROQ_API_KEY, model_name='mixtral-8x7b-32768')
+
+
+system = "You are a helpful assistant."
+human = "{text}"
+prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+
+chain = prompt | chat
+chain.invoke({"text": "Explain the importance of low latency LLMs."})
+
+
